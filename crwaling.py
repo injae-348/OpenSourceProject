cb880fa9ea480ad85774a00015054750759e772e
injae-348 <ant0593@naver.com>
2023-12-02 17:05:45 +0900

    Add Django project

--- crwaling.py
+++ crwaling.py
@@ -7,14 +7,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 
-class CRAWLING():
+class CRAWLING:
     def __init__(self) -> None:
         self.init_driver()
 
     def init_driver(self):
         self.chrome_service = Service(ChromeDriverManager().install())
         self.driver = None
-
 
     def main(self):
         self.driver = webdriver.Chrome(service=self.chrome_service)
@@ -23,13 +22,14 @@ class CRAWLING():
 
         s_input.send_keys("리트리버")
         s_input.send_keys(Keys.ENTER)
-        self.driver.find_element(By.XPATH,"//div[@class='btn_next _next']").click()
-        self.driver.find_elements(By.XPATH,"//div[@class='flick_bx']")[6].click()
+        self.driver.find_element(By.XPATH, "//div[@class='btn_next _next']").click()
+        self.driver.find_elements(By.XPATH, "//div[@class='flick_bx']")[6].click()
 
         news_titles = self.driver.find_elements(By.XPATH, "//a[@class='news_tit']")
         for index, value in enumerate(news_titles):
             print(f"{index+1} : {value.text}")
         pass
+
 
 if __name__ == "__main__":
     c = CRAWLING()
