147fa62f724274767cc49cecea47bc2802b8fcfc
injae-348 <ant0593@naver.com>
2023-12-03 16:06:41 +0900

    create a view for testing

--- petRecognizer/views.py
+++ petRecognizer/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from .forms import PetImageForm
 
 def index(request):
     return HttpResponse("어서오세요")
+
+def first_page(request):
+    if request.method=='POST':
+        form = PetImageForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            
+            # Image 업로드 후 YOLOV5 모델 여기서 적용!
+
+            return redirect('second_page')
+    else:
+        form = PetImageForm()
+
+    return render(request,'first.html',{'form':form})
