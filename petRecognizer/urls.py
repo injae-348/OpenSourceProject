bfc7e321cf4444ef4933009bb48ed9008fc27ebe
injae-348 <ant0593@naver.com>
2023-12-04 12:53:08 +0900

    add error page, i have to connect error page

--- petRecognizer/urls.py
+++ petRecognizer/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('pet/',views.first_page, name='first_page'),
     path('pet_result/',views.second_page,name='second_page'),
+    path('error/',views.error_page,name='error_page'),
 ]
--- petRecognizer/views.py
+++ petRecognizer/views.py
@@ -42,25 +42,28 @@ def first_page(request):
 
 
 def second_page(request):
+
     detection_result = request.session.get('detection_result')
 
     print("객체 탐지 결과 : ",detection_result)
 
     uploaded_image = detection_result['image_path']
     detected_class = detection_result['detections'][0]
-
+       
     relative_path = uploaded_image.replace(settings.MEDIA_ROOT,'').replace('\\','/') 
+    
     # image_path = os.path.join(settings.MEDIA_URL,relative_path)
-    image_path = '/'.join([settings.MEDIA_URL.rstrip('/'), relative_path.lstrip('/')])
-
-    # 품종에 대한 설명 만들어둔 DB에서 가져오기
-
+    image_path = '/'.join([settings.MEDIA_URL.rstrip('/'), relative_path.lstrip('/')])  
+    
+    # 품종에 대한 설명 만들어둔 DB에서 가져오기  
+    
     # 네이버 뉴스 헤드라인 가져오기
-
     context = {
-        'image_path':image_path,
-        'detected_class':detected_class,
+         'image_path':image_path,
+         'detected_class':detected_class,
     }
 
     return render(request,'second.html',context)
-
+    
+def error_page(request):
+    return render(request,'error.html')
