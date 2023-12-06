27bd6b6bc899d9a3596ec4e25c6bd7f741ad8256
Bukjin <PilseungGiGab@30AmoredBrigade>
2023-12-06 14:51:43 +0900

    commit 7 품종 리스트 완료

--- crwaling.py
+++ crwaling.py
@@ -35,25 +35,68 @@ class CRAWLING:
             s_input.send_keys("브리티시 쇼트헤어 고양이")
         elif breed == 'cat-Egyptian_Mau' :
             s_input.send_keys("이집션 마우 고양이")
-        elif breed == '3' :
-            s_input.send_keys("3")
-        elif breed == '3' :
-            s_input.send_keys("3")
-        elif breed == '3' :
-            s_input.send_keys("3")
-        elif breed == '3' :
-            s_input.send_keys("3")
-        elif breed == '3' :
-            s_input.send_keys("3")
-        elif breed == '3' :
-            s_input.send_keys("3")
-        elif breed == '3' :
-            s_input.send_keys("3")
-        elif breed == '3' :
-            s_input.send_keys("3")
-        elif breed == '3' :
-            s_input.send_keys("3")
-        
+        elif breed == 'cat-Maine_Coon' :
+            s_input.send_keys("메인쿤 고양이")
+        elif breed == 'cat-Persian' :
+            s_input.send_keys("페르시안 고양이")
+        elif breed == 'cat-Ragdoll' :
+            s_input.send_keys("랙돌 고양이")
+        elif breed == 'cat-Russian_Blue' :
+            s_input.send_keys("러시안 블루 고양이")
+        elif breed == 'cat-Siamese' :
+            s_input.send_keys("샴 고양이")
+        elif breed == 'cat-Sphynx' :
+            s_input.send_keys("스핑크스 고양이")
+        elif breed == 'dog-american_bulldog' :
+            s_input.send_keys("아메리칸 불독")
+        elif breed == 'dog-american_pit_bull_terrier' :
+            s_input.send_keys("아메리칸 핏불 테리어")
+        elif breed == 'dog-basset_hound' :
+            s_input.send_keys("바셋 하운드")
+        elif breed == 'dog-beagle' :
+            s_input.send_keys("비글")
+        elif breed == 'dog-boxer' :
+            s_input.send_keys("복서")
+        elif breed == 'dog-chihuahua' :
+            s_input.send_keys("치와와")
+        elif breed == 'dog-english_cocker_spaniel' :
+            s_input.send_keys("잉글리시 코커 스패니얼")
+        elif breed == 'dog-english_setter' :
+            s_input.send_keys("르웰린")
+        elif breed == 'dog-german_shorthaired' :
+            s_input.send_keys("저먼 쇼트헤어드 포인터")
+        elif breed == 'dog-great_pyrenees' :
+            s_input.send_keys("그레이트 피레니즈")
+        elif breed == 'dog-havanese' :
+            s_input.send_keys("하바나")
+        elif breed == 'dog-japanese_chin' :
+            s_input.send_keys("제페니스 친")
+        elif breed == 'dog-keeshond' :
+            s_input.send_keys("키스혼드")
+        elif breed == 'dog-leonberger' :
+            s_input.send_keys("레온베르거")
+        elif breed == 'dog-miniature_pinscher' :
+            s_input.send_keys("미니어처 핀셔")
+        elif breed == 'dog-newfoundland' :
+            s_input.send_keys("뉴펀들랜드")
+        elif breed == 'dog-pomeranian' :
+            s_input.send_keys("포메라니안")
+        elif breed == 'dog-pug' :
+            s_input.send_keys("퍼그")
+        elif breed == 'dog-saint_bernard' :
+            s_input.send_keys("세인트 버나드")
+        elif breed == 'dog-samoyed' :
+            s_input.send_keys("사모예드")
+        elif breed == 'dog-scottish_terrier' :
+            s_input.send_keys("스코티시 테리어")
+        elif breed == 'dog-shiba_inu' :
+            s_input.send_keys("시바 이누")
+        elif breed == 'dog-staffordshire_bull_terrier' :
+            s_input.send_keys("스타포드셔 불 테리어")
+        elif breed == 'dog-wheaten_terrier' :
+            s_input.send_keys("휘튼 테리어")
+        elif breed == 'dog-yorkshire_terrier' :
+            s_input.send_keys("요크셔테리어")
         else :
             print("검색할 수 없는 품종입니다.")
             return
